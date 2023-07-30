0a63649a470f7fe974ce15892cf2a6308469b3fb
harka <harka@Soni.mynetworksettings.com>
2023-07-29 22:36:03 -0400

    added comments

--- src/day2HW/day2HWDemo.java
+++ src/day2HW/day2HWDemo.java
@@ -2,25 +2,26 @@ package day2HW;
 
 public class day2HWDemo {
 
+	//pushed
 	public static void main(String[] args) {
 		System.out.println("Doing homework....");
 	}
 
-	
+	//pushed
 	public void getInfo() {
 		System.out.println("Homework is easy...");
 	}
-	
+	//pushed
 	public void getData() {
 		System.out.println("Homework is good practice...");
 	}
 
-	//added from github
+	//added from github and pulled to eclipse
 	public void add(int a, int b){
 		System.out.println(a + b);
 	}
 
-	//added from git
+	//added from git and pulled to eclipse
 	public void getInformation(){
 		System.out.println("Printing from GitHub");
 			}
